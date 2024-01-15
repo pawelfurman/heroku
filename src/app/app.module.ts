4f5e541a9690fb8d56481e9e1ac67fbc71412b36
pawelfurman <pawelfurman@pfurman-rm.lan>
2024-01-15 19:45:05 +0100

    Improve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenusComponent } from './menus/menus.component';
+import { RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenusComponent,
+    RecipesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'menus',
+        component: MenusComponent
+      },
+      {
+        path: 'recipes',
+        component: RecipesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
