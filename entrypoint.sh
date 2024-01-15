c9ff6fbad0be5ff077030e81049f66750b508fe1
pawelfurman <pawelfurman@pfurman-rm.lan>
2024-01-15 20:25:03 +0100

    Improve

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
+
+sed -i "s%ENVIRONMENT%#$ENVIRONMENT#g" ./src/environments/environment.template.ts
+
+
 rm ./src/environments/environment.prod.ts
 cp ./src/environments/environment.template.ts ./src/environments/environment.prod.ts
