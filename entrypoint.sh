1883a19114095b64f2fca003c2abb36c9ffa664c
pawelfurman <pawelfurman@pfurman-rm.lan>
2024-01-15 20:46:08 +0100

    Improve

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ echo 123
 echo $ENVIRONMENT
 echo $NPM_CONFIG_LOGLEVEL
 
-sed -i 's#%ENVIRONMENT%#$ENVIRONMENT#g' ./src/environments/environment.template.ts
+sed -i "s#%ENVIRONMENT%#$ENVIRONMENT#g" ./src/environments/environment.template.ts
 
 rm ./src/environments/environment.prod.ts
 cp ./src/environments/environment.template.ts ./src/environments/environment.prod.ts
